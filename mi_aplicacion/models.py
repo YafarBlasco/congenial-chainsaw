5aff7c39db67b9f579bfedd1d87008c44bfdf76b
YafarBlasco <78188289+YafarBlasco@users.noreply.github.com>
2023-06-29 21:07:33 -0300

    Update models.py

--- mi_aplicacion/models.py
+++ mi_aplicacion/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Clase1(models.Model):
+    campo1 = models.CharField(max_length=50)
+    campo2 = models.IntegerField()
+
+class Clase2(models.Model):
+    campo3 = models.CharField(max_length=50)
+    campo4 = models.CharField(max_length=100)
+
+class Clase3(models.Model):
+    campo5 = models.DateField()
+    campo6 = models.BooleanField()
